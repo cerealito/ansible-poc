db4eb000fb6b1277c2493a194234d4873a823375
Samuel Flores <samuelflores@gmail.com>
2016-01-21 00:29:06 +0100

    First view for the charts app :)

--- src/monitoring/urls.py
+++ src/monitoring/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'monitoring.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^charts/', include('charts.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
