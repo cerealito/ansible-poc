9964b02ce9563646ae3996f24a567989e030a0a5
Samuel Flores <samuelflores@gmail.com>
2016-01-22 16:34:51 +0100

    shortcut for render

--- src/charts/views.py
+++ src/charts/views.py
@@ -1,14 +1,14 @@
 from charts.models import Host
 from django.http import HttpResponse
-from django.template import loader, RequestContext
+from django.shortcuts import render
 
 
 def index(request):
     all_hosts_l = Host.objects.all()
 
-    template = loader.get_template('charts/index.html')
-    context = RequestContext(request, {'host_l': all_hosts_l})
-    return HttpResponse(template.render(context))
+    context = {'host_l': all_hosts_l}
+    # the render shortcut returns an HttpResponse object
+    return render(request, 'charts/index.html', context)
 
 
 def host(request, host_id):
