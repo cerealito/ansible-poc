e6856330972fc8b86d2cdb3870becf8e9b10297c
Samuel Flores <samuelflores@gmail.com>
2016-01-22 18:37:17 +0100

    Adding a graph template. Using pygal for charts.

--- src/charts/views.py
+++ src/charts/views.py
@@ -1,5 +1,5 @@
-from charts.models import Host
-from django.http import HttpResponse
+import pygal
+from charts.models import Host, MemUsageSample
 from django.shortcuts import render
 
 
@@ -12,4 +12,17 @@ def index(request):
 
 
 def host(request, host_id):
-    return HttpResponse("show charts for host %s" % host_id)
+    sample_l = MemUsageSample.objects.filter(host_id=host_id)
+
+    mem_chart = pygal.Line(x_label_rotation=30)
+    time_labels = []
+    mem_values = []
+    for s in sample_l:
+        assert isinstance(s, MemUsageSample)
+        time_labels.append(s.datetime)
+        mem_values.append(s.num_mb)
+
+    mem_chart.x_labels = time_labels
+    mem_chart.add('Free memory', mem_values)
+
+    return render(request, 'charts/graphs.html', {'chart_rendition': mem_chart.render()})
