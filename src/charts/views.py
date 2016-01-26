f7ddd677376d8e1450436c948522b8f25316ebd4
Samuel Flores <samuelflores@gmail.com>
2016-01-26 15:01:30 +0100

    View now adds disk usage series to the output graph

--- src/charts/views.py
+++ src/charts/views.py
@@ -1,5 +1,5 @@
 import pygal
-from charts.models import Host, MemUsageSample
+from charts.models import Host, MemUsageSample, FSUsageSample
 from django.http import Http404
 from django.shortcuts import render
 
@@ -18,20 +18,32 @@ def host(request, host_id):
 
     current_host = Host.objects.filter(pk=host_id)[0]
 
-    sample_l = MemUsageSample.objects.filter(host_id=host_id)
+    mem_sample_l = MemUsageSample.objects.filter(host_id=host_id)
+    fsu_sample_l = FSUsageSample.objects.filter(host_id=host_id)
 
-    mem_chart = pygal.Line(title='Percentage of free memory in ' + current_host.name,
+    mem_chart = pygal.Line(title='Hardware resources in ' + current_host.name,
                            x_label_rotation=30,
                            range=(0, 100),
-                           show_legend=False)
-    time_labels = []
+                           legend_box_size=20)
+    mem_times = []
     mem_values = []
-    for s in sample_l:
+    for s in mem_sample_l:
         assert isinstance(s, MemUsageSample)
-        time_labels.append(s.datetime)
+        mem_times.append(s.datetime)
         mem_values.append(s.percent)
 
-    mem_chart.x_labels = time_labels
-    mem_chart.add('Free memory', mem_values)
+    mem_chart.x_labels = mem_times
+    mem_chart.add('Used memory (%)', mem_values)
+
+    fs_times = []
+    fs_values = []
+    for s in fsu_sample_l:
+        assert isinstance(s, FSUsageSample)
+        fs_times.append(s.datetime)
+        fs_values.append(s.percent)
+
+    mem_chart.x_labels = fs_times
+    mem_chart.add('Disk Usage (%)', fs_values)
+
 
     return render(request, 'charts/graphs.html', {'chart_rendition': mem_chart.render()})
