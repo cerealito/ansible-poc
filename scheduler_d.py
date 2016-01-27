1dd725be43d9f54782022cad7ec4040ee5605bf2
Samuel Flores <samuelflores@gmail.com>
2016-01-27 18:45:06 +0100

    Broken: trying to use APS to schedule tasks

--- scheduler_d.py
+++ scheduler_d.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from datetime import datetime
+import time
+import os
+from apscheduler.schedulers.background import BackgroundScheduler
+
 __author__ = 'Samuel Flores'
 
 if __name__ == '__main__':
@@ -14,9 +20,31 @@ if __name__ == '__main__':
 
 from pbwrapper import PBExecutorWrapper
 
-pb = './playbook.yml'
-i = './inventory.txt'
 
-pbe = PBExecutorWrapper(pb, i)
-pbe.run()
+def call_ansible():
+    print('Tick! The time is: %s' % datetime.now())
+
+    pb = './playbook.yml'
+    i = './inventory.txt'
+
+    pbe = PBExecutorWrapper(pb, i)
+    pbe.run()
+
+
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(call_ansible, 'interval', seconds=60)
+    scheduler.start()
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(60*60)
+            print('main thread slept for an hour')
+
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
+
 
