d2f05704b519bac4143de77cfc1ba1bcc0e79fb0
Samuel Flores <samuelflores@gmail.com>
2016-01-28 17:27:47 +0100

    redirection from server:8000/ to server:8000/charts

--- src/monitoring/urls.py
+++ src/monitoring/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'monitoring.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', lambda r: HttpResponseRedirect('charts/')),
     url(r'^charts/', include('charts.urls', namespace='charts')),
     url(r'^admin/', include(admin.site.urls)),
 )
